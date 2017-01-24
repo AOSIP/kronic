59804981faac9385ff9bacd4243a93c9cf518b45
Akhil Narang <akhilnarang.1999@gmail.com>
2017-01-24 10:06:13 +0530

    kronic: add in scripts
    Change-Id: I29b1c63a742f748b37524275ecbc0bb0a507c9cc
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+export logfile="kronic/logs/${device}/$(date +%Y%m%d-%H%M).log";
+export AOSIP_BUILDTYPE="Official";
+mkdir -pv ./kronic/logs/${device};
+. build/envsetup.sh >> $logfile;
+rm -rf .repo/local_manifests;
+./sync.sh >> $logfile;
+make clobber 2>&1 | tee -a $logfile;
+export device=$1
+if [ -z $device ];
+	then
+	echo -e "No device found, exiting!" >> $logfile
+	exit 1
+fi
+lunch aosip_$device-userdebug 2>&1 | tee -a $logfile
+export USE_CCACHE=1
+export CCACHE_DIR=${HOME}/.ccache-${device}
+ccache -M 25G >> $logfile
+if [ -z $THREAD ];
+	then
+	export THREAD=$(grep -c "^processor" /proc/cpuinfo)
+fi
+time make -j8 kronic 2>&1 | tee -a ${logfile};
+if [ "$(ls ${OUT}/AOSiP*.zip 2> /dev/null | wc -l)" != "0" ];
+	then
+	echo "BUILD SUCCESFUL" >> $logfile;
+	rsync -av $OUT/A* kronic@aosip.xyz:downloads.aosip.xyz/n-mr1/${device} >> $logfile;
+else
+	echo "BUILD FAILED" >> $logfile;
+	echo "Check for errors in ${logfile}";
+fi
+
+echo -e "Stopping jack server" >> $logfile;
+./prebuilts/sdk/tools/jack-admin stop-server >> $logfile;
+
+echo -e "Pushing logs up!";
+cd kronic;
+git add -A; git commit -asm $(date +%Y%m%d); git fetch origin master; git rebase origin/master; git push origin master;
+cd -;
+
