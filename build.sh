8d6a5ae8d79a064dbbf171520dd3f1dc7d018c78
Akhil Narang <akhilnarang.1999@gmail.com>
2017-01-31 06:55:42 -0500

    kronic: 5.5-r1 Weekly

--- build.sh
+++ build.sh
@@ -6,21 +6,16 @@ if [ -z $device ];
 	exit 1
 fi
 export logfile="kronic/logs/${device}/$(date +%Y%m%d-%H%M).log";
-export AOSIP_BUILDTYPE="Official";
+export AOSIP_BUILDTYPE="Weekly";
 mkdir -pv ./kronic/logs/${device};
 . build/envsetup.sh >> $logfile;
 rm -rf .repo/local_manifests;
 ./sync.sh >> $logfile;
-make clobber 2>&1 | tee -a $logfile;
 lunch aosip_$device-userdebug
 export USE_CCACHE=1
 export CCACHE_DIR=${HOME}/.ccache-${device}
 ccache -M 25G >> $logfile
-if [ -z $THREAD ];
-	then
-	export THREAD=$(grep -c "^processor" /proc/cpuinfo)
-fi
-time make -j8 kronic 2>&1 | tee -a ${logfile};
+time mka kronic 2>&1 | tee -a ${logfile};
 if [ "$(ls ${OUT}/AOSiP*.zip 2> /dev/null | wc -l)" != "0" ];
 	then
 	echo "BUILD SUCCESFUL" >> $logfile;
